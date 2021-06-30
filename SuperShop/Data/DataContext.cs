c329509e0ac8ec004abbfd0befb81cae612cc2b1
IsabellaMateus <cl.m.mateus@gmail.com>
2021-06-30 12:21:02 +0100

    First controller

---
+++ SuperShop/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+
+namespace SuperShop.Data
+{
+    //Herança (o meu DataContext vai herdar do DbContext da Microsoft.EntityFrameworkCore)
+    public class DataContext : DbContext
+    {
+        //propriedade responsável pela tabela de produtos
+        public DbSet<Product> Products { get; set; }
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ SuperShop/Data/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperShop.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        //formato moeda com duas casas decimais mas em modo edição não formata nada, deixa a pessoa escrever
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image")] //Para aparecer 'Image' escrito na página da WEB
+        public string ImageUrl { get; set; } //link da imagem do produto
+
+        //ultima compra
+        [Display(Name = "Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+
+
+        //ultima venda
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+        //para saber se o produto está disponivel ou não
+        [Display(Name = "Is Available")]
+        public bool IsAvailable { get; set; }
+
+        //formato número com duas casas decimais
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+}
